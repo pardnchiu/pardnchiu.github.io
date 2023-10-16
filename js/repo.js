083829c36b43f425848a6b2216ca4791dd03e6d8
Remote <develper@pardn.io>
2023-10-16 09:55:48 +0800

    clear commits.

---
+++ js/repo.js
@@ -0,0 +1,296 @@
+const repo = [
+    {
+        sn: 42,
+        href: "https://github.com/pardnchiu/social-networking",
+        src: "https://pardn.io/image/repo/social-networking.jpg",
+        title: "Social Networking 社群平台實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 41,
+        href: "https://github.com/pardnchiu/minimal-blog",
+        src: "https://pardn.io/image/repo/minimal-blog.jpg",
+        title: "Minimal Blog 單頁式部落格實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 40,
+        href: "https://github.com/pardnchiu/classic-blog",
+        src: "https://pardn.io/image/repo/classic-blog.jpg",
+        title: "Classic Blog 經典樣式部落格實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 39,
+        href: "https://github.com/pardnchiu/personal-blog-with-cover",
+        src: "https://pardn.io/image/repo/personal-blog-with-cover.jpg",
+        title: "Personal Blog with Cover個人部落格 (含封面圖) 實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 38,
+        href: "https://github.com/pardnchiu/personal-blog",
+        src: "https://pardn.io/image/repo/personal-blog.jpg",
+        title: "Personal Blog 個人部落格實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 37,
+        href: "https://github.com/pardnchiu/magazine-blog",
+        src: "https://pardn.io/image/repo/magazine-blog.jpg",
+        title: "Magazine Blog 雜誌式部落格實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 36,
+        href: "https://github.com/pardnchiu/simplify-blog",
+        src: "https://pardn.io/image/repo/simplify-blog.jpg",
+        title: "Simplify Blog 簡易式部落格實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 35,
+        href: "https://github.com/pardnchiu/app-introduction-page",
+        src: "https://pardn.io/image/repo/app-introduction-page.jpg",
+        title: "App Introduction Page App 介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 34,
+        href: "https://github.com/pardnchiu/pokemon-quest-css-drawing",
+        src: "https://pardn.io/image/repo/pokemon-quest-css-drawing.jpg",
+        title: "Pokemon Quest Css Drawing 寶可夢探險圖鑑 (CSS繪圖)",
+        tag: "CSS繪圖"
+    },
+    {
+        sn: 33,
+        href: "https://github.com/pardnchiu/personal-page-with-blog",
+        src: "https://pardn.io/image/repo/personal-page-with-blog.jpg",
+        title: "Personal Page with Blog 個人介紹頁 (含部落格) 實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 32,
+        href: "https://github.com/pardnchiu/gym-introduction-page",
+        src: "https://pardn.io/image/repo/gym-introduction-page.jpg",
+        title: "Gym Introduction Page 健身房介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 31,
+        href: "https://github.com/pardnchiu/my-profile-page",
+        src: "https://pardn.io/image/repo/my-profile-page.jpg",
+        title: "My Profile Page 我的個人資料頁實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 30,
+        href: "https://github.com/pardnchiu/about-me-page",
+        src: "https://pardn.io/image/repo/about-me-page.jpg",
+        title: "About Me Page 關於我頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 29,
+        href: "https://github.com/pardnchiu/self-introduction-page",
+        src: "https://pardn.io/image/repo/self-introduction-page.jpg",
+        title: "Self Introduction Page 自我介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 28,
+        href: "https://github.com/pardnchiu/personal-page",
+        src: "https://pardn.io/image/repo/personal-page.jpg",
+        title: "Personal Page 個人頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 27,
+        href: "https://github.com/pardnchiu/service-introduction-page",
+        src: "https://pardn.io/image/repo/service-introduction-page.jpg",
+        title: "Service Introduction Page 服務介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 26,
+        href: "https://github.com/pardnchiu/restaurant-introduction-page",
+        src: "https://pardn.io/image/repo/restaurant-introduction-page.jpg",
+        title: "Restaurant Introduction Page 餐廳介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 25,
+        href: "https://github.com/pardnchiu/firebase-messaging-ios",
+        src: "https://pardn.io/image/repo/firebase-messaging-ios.jpg",
+        title: "Firebase Messaging 即時訊息 (iOS) 實作範例",
+        tag: "iOS"
+    },
+    {
+        sn: 24,
+        href: "https://github.com/pardnchiu/moneybook-ios",
+        src: "https://pardn.io/image/repo/moneybook-ios.jpg",
+        title: "Moneybook 簡易記帳 (iOS) 實作範例",
+        tag: "iOS"
+    },
+    {
+        sn: 23,
+        href: "https://github.com/pardnchiu/personal-profile-page",
+        src: "https://pardn.io/image/repo/personal-profile-page.jpg",
+        title: "Personal Profile Page 個人介紹頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 22,
+        href: "https://github.com/pardnchiu/moneybook",
+        src: "https://pardn.io/image/repo/moneybook.jpg",
+        title: "Moneybook 簡易記帳實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 21,
+        href: "https://github.com/pardnchiu/my-portfolio-page",
+        src: "https://pardn.io/image/repo/my-portfolio-page.jpg",
+        title: "My Portfolio Page 我的作品頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 20,
+        href: "https://github.com/pardnchiu/personal-portfolio-page",
+        src: "https://pardn.io/image/repo/personal-portfolio-page.jpg",
+        title: "Personal Portfolio Page 個人作品頁面實作範例",
+        tag: "Web"
+    },
+    {
+        sn: 19,
+        href: "https://github.com/pardnchiu/website-template-15",
+        src: "https://pardn.io/image/repo/website-template-15.jpg",
+        title: "Website Template 網站模板 15",
+        tag: "Web"
+    },
+    {
+        sn: 18,
+        href: "https://github.com/pardnchiu/website-template-14",
+        src: "https://pardn.io/image/repo/website-template-14.jpg",
+        title: "Website Template 網站模板 14",
+        tag: "Web"
+    },
+    {
+        sn: 17,
+        href: "https://github.com/pardnchiu/website-template-13",
+        src: "https://pardn.io/image/repo/website-template-13.jpg",
+        title: "Website Template 網站模板 13",
+        tag: "Web"
+    },
+    {
+        sn: 16,
+        href: "https://github.com/pardnchiu/website-template-12",
+        src: "https://pardn.io/image/repo/website-template-12.jpg",
+        title: "Website Template 網站模板 12",
+        tag: "Web"
+    },
+    {
+        sn: 15,
+        href: "https://github.com/pardnchiu/website-template-11",
+        src: "https://pardn.io/image/repo/website-template-11.jpg",
+        title: "Website Template 網站模板 11",
+        tag: "Web"
+    },
+    {
+        sn: 14,
+        href: "https://github.com/pardnchiu/website-template-10",
+        src: "https://pardn.io/image/repo/website-template-10.jpg",
+        title: "Website Template 網站模板 10",
+        tag: "Web"
+    },
+    {
+        sn: 13,
+        href: "https://github.com/pardnchiu/website-template-9",
+        src: "https://pardn.io/image/repo/website-template-9.jpg",
+        title: "Website Template 網站模板 9",
+        tag: "Web"
+    },
+    {
+        sn: 12,
+        href: "https://github.com/pardnchiu/website-template-8",
+        src: "https://pardn.io/image/repo/website-template-8.jpg",
+        title: "Website Template 網站模板 8",
+        tag: "Web"
+    },
+    {
+        sn: 11,
+        href: "https://github.com/pardnchiu/website-template-7",
+        src: "https://pardn.io/image/repo/website-template-7.jpg",
+        title: "Website Template 網站模板 7",
+        tag: "Web"
+    },
+    {
+        sn: 10,
+        href: "https://github.com/pardnchiu/instagram-202210-web",
+        src: "https://pardn.io/image/repo/instagram-202210-web.jpg",
+        title: "Instagram 仿製 (Web)",
+        tag: "Web"
+    },
+    {
+        sn: 9,
+        href: "https://github.com/pardnchiu/markdown-editor",
+        src: "https://pardn.io/image/repo/markdown-editor-web.jpg",
+        title: "Markdown 編輯器 (Web)",
+        tag: "Web"
+    },
+    {
+        sn: 8,
+        href: "https://github.com/pardnchiu/firebase-messager",
+        src: "https://pardn.io/image/repo/firebase-messager-web.jpg",
+        title: "Firebase 即時訊息 (Web)",
+        tag: "Web"
+    },
+    {
+        sn: 7,
+        href: "https://github.com/pardnchiu/management-1-web",
+        src: "https://pardn.io/image/repo/management-1-web.jpg",
+        title: "後台管理 1 (Web)",
+        tag: "Web"
+    },
+    {
+        sn: 6,
+        href: "https://github.com/pardnchiu/website-template-6",
+        src: "https://pardn.io/image/repo/website-template-6.jpg",
+        title: "Website Template 網站模板 6",
+        tag: "Web"
+    },
+    {
+        sn: 5,
+        href: "https://github.com/pardnchiu/website-template-5",
+        src: "https://pardn.io/image/repo/website-template-5.jpg",
+        title: "Website Template 網站模板 5",
+        tag: "Web"
+    },
+    {
+        sn: 4,
+        href: "https://github.com/pardnchiu/website-template-4",
+        src: "https://pardn.io/image/repo/website-template-4.jpg",
+        title: "Website Template 網站模板 4",
+        tag: "Web"
+    },
+    {
+        sn: 3,
+        href: "https://github.com/pardnchiu/website-template-3",
+        src: "https://pardn.io/image/repo/website-template-3.jpg",
+        title: "Website Template 網站模板 3",
+        tag: "Web"
+    },
+    {
+        sn: 2,
+        href: "https://github.com/pardnchiu/website-template-2",
+        src: "https://pardn.io/image/repo/website-template-2.jpg",
+        title: "Website Template 網站模板 2",
+        tag: "Web"
+    },
+    {
+        sn: 1,
+        href: "https://github.com/pardnchiu/website-template-1",
+        src: "https://pardn.io/image/repo/website-template-1.jpg",
+        title: "Website Template 網站模板 1",
+        tag: "Web"
+    }
+];
