7b8b9588ed622301930cdb06cfa8ff711ff3ab69
Pardn Chiu <dev@pardn.io>
2025-09-10 12:07:56 +0800

    refactor: 新增 `sh/*`

---
+++ sh/debian_13.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -e
+
+# 設定套件源
+echo "設定套件源"
+sudo tee /etc/apt/sources.list.d/debian.sources > /dev/null << 'EOF'
+Types: deb
+URIs: http://deb.debian.org/debian/
+Suites: trixie trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb
+URIs: http://security.debian.org/debian-security/
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
+
+# 更新套件清單
+echo "更新套件"
+sudo apt update
+
+# 升級系統
+echo "升級套件"
+sudo apt upgrade -y
+
+# 安裝基本套件
+echo "安裝基本套件"
+sudo apt install ufw rsync wget curl tar zip unzip nano vim git sysbench stress-ng htop iotop bc fio qemu-guest-agent --fix-missing -y
+
+# 啟用 QEMU Guest Agent
+echo "啟用 QEMU Guest Agent"
+sudo systemctl enable qemu-guest-agent
+
+# 設定語系
+echo "設定語系"
+sudo localectl set-locale LANG=en_US.UTF-8
+
+# 設定時區
+echo "設定時區"
+sudo timedatectl set-timezone Asia/Taipei
+
+# 重新配置 dash
+echo "重新配置 dash"
+sudo dpkg-reconfigure -f noninteractive dash
+
+# 設定系統參數
+echo "設定系統參數"
+echo "net.ipv4.icmp_echo_ignore_all = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf > /dev/null
+echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -p
+
+# 更新 GRUB 設定
+echo "更新 GRUB 設定"
+sudo sed -i -e "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"ipv6.disable=1 /g" /etc/default/grub
+sudo update-grub
+
+# 建立 swap 檔案
+echo "建立 swap"
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# 下載並設定系統資訊腳本
+echo "設定系統資訊腳本"
+sudo curl -f -o /usr/local/bin/sysinfo https://gist.githubusercontent.com/pardnchiu/561ef0581911eac7aed33c898a1a2b21/raw/4859f901ed986522fd7a4b694a513f0716a3c7ac/sysinfo
+sudo chmod +x /usr/local/bin/sysinfo
+sudo cp -f /usr/local/bin/sysinfo /etc/update-motd.d/99-custom
+sudo chmod +x /etc/update-motd.d/99-custom
+sudo run-parts /etc/update-motd.d/
+
+# 執行測試腳本
+echo "執行效能測試"
+curl -s https://gist.githubusercontent.com/pardnchiu/8f23f9e98af9af4c9b1e139816a21118/raw/eadcc04a45cd5cefa0ad97403326dd022176ac69/test | bash
+
+sudo reboot
